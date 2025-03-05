bba383b722a25439623712ee4f75665d1e200b6f
areebkaz <areebkazia@gmail.com>
2025-03-05 15:57:44 +0530

    astronomy

---
+++ src/app/astronomy/axial-tilt/page.tsx
@@ -0,0 +1,14 @@
+// src/app/astronomy/axial-tilt/page.tsx
+import React from 'react';
+
+const AxialTiltPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Axial Tilt of the Earth</h1>
+      <p>The axial tilt of the Earth is the angle between the Earth's rotational axis and its orbital axis.</p>
+      {/* Add your content here */}
+    </div>
+  );
+}
+
+export default AxialTiltPage;
